a2bef0c8c8ccdbcde768273bf26862be9d6f985a
Narawit Triprom <narawittriprom@gmail.com>
2020-11-04 02:52:48 +0700

    add animation and enable pwa

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = {
-    images:{
-        domains:['s3-alpha-sig.figma.com']
-    }
-}
-// withPWA({
-//   pwa: {
-//     dest: "public",
-//     runtimeCaching,
-//   },
-// });
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+  },
+  images: {
+    domains: ["s3-alpha-sig.figma.com"],
+  },
+});
--- pages/recepie/[id].tsx
+++ pages/recepie/[id].tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion"
 import { useRouter } from "next/router"
 import { FaChevronLeft, FaDog, FaRegCheckCircle, FaRegHeart } from "react-icons/fa"
 
@@ -5,7 +6,8 @@ const RecepieDetail = () => {
     const Router = useRouter()
     return <>
         <img className="w-screen" style={{ transform: "translateY(-30%)", }} src="https://s3-alpha-sig.figma.com/img/ba95/7ae4/19bc1954bc477640abb1f855c9085871?Expires=1605484800&Signature=CDBQZjDG~SDufyKOkLlOWlremDUVpKC2yyNx-pvoxW1Cv0uiFAA4suS8xt1S~9gERg6Mrf7D99ajRZSG09wUue4MndnCGuOvbDb8KY~tIMJdgr2eFgj6mH9IA9pPD6yFeSJ0qQCM7BEBRuDw0FCWfH8s0iZWyw4VXUnkwyR0x8wzEtRunz7pTMuYndhYLKbu3w7ldqa~gH9vZAI2FsVSG6DIIOndMwmnQYOFbEAO1RvMLw-3eQc1ssjkhkdNCQp~1hcdf~T3MMJvdX44La6BY1kcNNAM~y~JJJJq5ZPafYda6CEcBewxG-2-TuvFT0ZW5SGD32PGUkB4bYFccDjYfQ__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"></img>
-        <div className="fixed top-0 left-0 w-screen h-screen flex flex-col">
+        <motion.div animate={{ opacity: 1 }} transition={{ duration: 0.2 }}
+            style={{ opacity: 0 }} className="fixed top-0 left-0 w-screen h-screen flex flex-col">
             <div className="w-screen px-4 mt-8">
                 <div className="flex">
                     <button onClick={() => { Router.push('/recepie') }} className="rounded-full bg-white shadow-md p-2">
@@ -55,7 +57,7 @@ const RecepieDetail = () => {
                     <p>Egg <span className="float-right">3 %</span></p>
                 </div>
             </div>
-        </div>
+        </motion.div>
     </>
 }
 
