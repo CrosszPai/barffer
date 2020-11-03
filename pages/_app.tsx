af07fd30e4238077b1de45158149634220e800e6
Narawit Triprom <narawittriprom@gmail.com>
2020-11-03 20:19:51 +0700

    fix apple status bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
         name="viewport"
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
       />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="black"></meta>
       <link rel="shortcut icon" href="/favicon.ico" />
       <title>Barffer</title>
       <link rel="manifest" href="/manifest.json" />
