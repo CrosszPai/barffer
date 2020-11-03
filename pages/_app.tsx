69683e6b0cf0452c7d1e41c86cdee26c00fa76e4
Narawit Triprom <narawittriprom@gmail.com>
2020-11-03 20:18:17 +0700

    add apple bookmark icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,9 +12,10 @@ function MyApp({ Component, pageProps }) {
         content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
       />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"></meta>
-       <link rel="shortcut icon" href="/favicon.ico" />
-       <title>Barffer</title>
-       <link rel="manifest" href="/manifest.json" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <title>Barffer</title>
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" sizes="128x128" href="/icons/icon-128x128.png"></link>
     </Head>
     <div className=" w-screen bg-barffer-bg overflow-auto" style={{
       height: 'calc(100vh - 3.5rem)'
