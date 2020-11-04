de0743bc202751191676e469a7286062fc9c2c5c
Narawit Triprom <narawittriprom@gmail.com>
2020-11-04 13:47:52 +0700

    fix Link issue

--- components/nav-bottom/index.tsx
+++ components/nav-bottom/index.tsx
@@ -6,16 +6,24 @@ const NavBottom = () => {
     const Router = useRouter()
     return <nav className={styles['nav-container']}>
         <Link href='/'>
-            <FaHome size="1.25rem" className={Router.asPath === '/' && styles['link-active']} />
+            <a className="flex-1 flex justify-center">
+                <FaHome size="1.25rem" className={Router.asPath === '/' && styles['link-active']} />
+            </a>
         </Link>
         <Link href="/recepie">
-            <FaList size="1.25rem" className={Router.asPath === '/recepie' && styles['link-active']} />
+            <a className="flex-1 flex justify-center">
+                <FaList size="1.25rem" className={Router.asPath === '/recepie' && styles['link-active']} />
+            </a>
         </Link>
-        <Link href="/fav">
-            <FaHeart size="1.25rem" className={Router.asPath === '/fav' && styles['link-active']} />
+        <Link href="/favorite">
+            <a className="flex-1 flex justify-center">
+                <FaHeart size="1.25rem" className={Router.asPath === '/fav' && styles['link-active']} />
+            </a>
         </Link>
         <Link href="/cal">
-            <FaCalculator size="1.25rem" className={Router.asPath === '/cal' && styles['link-active']} />
+            <a className="flex-1 flex justify-center">
+                <FaCalculator size="1.25rem" className={Router.asPath === '/cal' && styles['link-active']} />
+            </a>
         </Link>
     </nav>
 }
