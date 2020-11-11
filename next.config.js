48bb467f1205f033d459c06ea65fa4cc2bb80fca
narawit triprom <61010556@kmitl.ac.th>
2020-11-12 03:43:00 +0700

    enable pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//     runtimeCaching,
-//   },
-// images: {
-//   domains: ["s3-alpha-sig.figma.com"],
-// },
-// });
-
-module.exports = {
-  images: {
-    domains: ["s3-alpha-sig.figma.com"],
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
   },
-}
+images: {
+  domains: ["s3-alpha-sig.figma.com"],
+},
+});
+
+// module.exports = {
+//   images: {
+//     domains: ["s3-alpha-sig.figma.com"],
+//   },
+// }
